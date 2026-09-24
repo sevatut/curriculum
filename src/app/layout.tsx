c65be5f77227113bd0b4b6cc04a69847aa789ab3
unknown <375291936581@yandex.by>
2026-09-24 17:55:32 +0300

    feat: apply brand theme and Roboto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,15 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 import { headers } from 'next/headers'
 
 import { defaultLocale } from '@/shared/i18n'
 
 import './globals.css'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const roboto = Roboto({
+  variable: '--font-roboto',
   subsets: ['latin', 'cyrillic'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
 })
 
 export const metadata: Metadata = {
@@ -32,10 +28,7 @@ export default async function RootLayout({
   const lng = headersList.get('x-i18next-current-language') ?? defaultLocale
 
   return (
-    <html
-      lang={lng}
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang={lng} className={`${roboto.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col font-sans">{children}</body>
     </html>
   )
